2538d4105a4b07321cdc6f35ea42cc3509a910da
erjigit12 <bvjcjcgjf@gmail.com>
2025-01-26 16:44:43 +0600

    sender_subscription_page add

---
+++ lib/src/feature/deliver/presentation/pages/sender_subscription_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SenderSubscriptionPage extends StatelessWidget {
+  const SenderSubscriptionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
