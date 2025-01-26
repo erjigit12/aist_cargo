e7d641e6b10dbe4ba0ae9e0d0b45ac5071612c2b
erjigit12 <bvjcjcgjf@gmail.com>
2025-01-26 17:24:58 +0600

    sender_subscription_page full

--- lib/src/core/widgets/elevated_button_widget.dart
+++ lib/src/core/widgets/elevated_button_widget.dart
@@ -19,14 +19,18 @@ class ElevatedButtonWidget extends StatelessWidget {
           child: ElevatedButton(
             onPressed: onPressed,
             style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.buttonColor,
-              fixedSize: const Size(double.infinity, 60),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
+                backgroundColor: AppColors.buttonColor,
+                fixedSize: const Size(double.infinity, 60),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                textStyle: AppTextStyles.f18w400),
+            child: Text(
+              title,
+              style: AppTextStyles.f20w400.copyWith(
+                color: const Color(0xFF101010),
               ),
-              textStyle: AppTextStyles.f18w400,
             ),
-            child: Text(title),
           ),
         ),
       ],
--- lib/src/feature/deliver/presentation/pages/sender_subscription_page.dart
+++ lib/src/feature/deliver/presentation/pages/sender_subscription_page.dart
@@ -1,8 +1,16 @@
 import 'package:aist_cargo/src/core/core.dart';
+import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:flutter/material.dart';
 
-class SenderSubscriptionPage extends StatelessWidget {
+class SenderSubscriptionPage extends StatefulWidget {
   const SenderSubscriptionPage({super.key});
+
+  @override
+  State<SenderSubscriptionPage> createState() => _SenderSubscriptionPageState();
+}
+
+class _SenderSubscriptionPageState extends State<SenderSubscriptionPage> {
+  int selectedSubscriptionIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -10,24 +18,77 @@ class SenderSubscriptionPage extends StatelessWidget {
       appBar: const CustomAppBar(
         title: 'Подключить Подписку',
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/sender.png'),
-          16.h,
-          const Text(
-            'Вы выбрали тариф: 1 месяц за 99₽',
-            style: AppTextStyles.f16w500,
-            textAlign: TextAlign.center,
-          ),
-          16.h,
-          const Text(
-            ' Чем больше срок, тем больше выгода',
-            style: AppTextStyles.f12w400,
-            textAlign: TextAlign.center,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Image.asset('assets/images/sender.png'),
+            16.h,
+            const Text(
+              'Вы выбрали тариф: 1 месяц за 99₽',
+              style: AppTextStyles.f16w500,
+              textAlign: TextAlign.center,
+            ),
+            16.h,
+            const Text(
+              ' Чем больше срок, тем больше выгода',
+              style: AppTextStyles.f12w400,
+              textAlign: TextAlign.center,
+            ),
+            8.h,
+            SubscriptionButton(
+              text: 'Подключить за 999Р на 3 месяцев',
+              color: AppColors.subscribeBlueColor,
+              index: 2,
+              currentIndex: 1,
+              onSelect: (index) {
+                // setModalState(() {
+                //   localSelectedSubscriptionIndex = index;
+                // });
+                // setState(() {
+                //   selectedSubscriptionIndex = index;
+                // });
+              },
+            ),
+            12.h,
+            SubscriptionButton(
+              text: 'Подключить за 1499Р на 6 месяцев',
+              color: AppColors.subscribeRedColor,
+              index: 3,
+              currentIndex: 1,
+              onSelect: (index) {
+                // setModalState(() {
+                //   localSelectedSubscriptionIndex = index;
+                // });
+                // setState(() {
+                //   selectedSubscriptionIndex = index;
+                // });
+              },
+            ),
+            12.h,
+            SubscriptionButton(
+              text: 'Подключить за 1999Р на 12 месяцев',
+              color: AppColors.subscribeYellowColor,
+              index: 4,
+              currentIndex: 1,
+              onSelect: (index) {
+                // setModalState(() {
+                //   localSelectedSubscriptionIndex = index;
+                // });
+                // setState(() {
+                //   selectedSubscriptionIndex = index;
+                // });
+              },
+            ),
+            8.h,
+            const Spacer(),
+            ElevatedButtonWidget(title: 'Далее', onPressed: () {}),
+            30.h,
+          ],
+        ),
       ),
     );
   }
