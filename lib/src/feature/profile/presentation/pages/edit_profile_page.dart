537b682c8fe0c25ad8ee99ca43c6e5cb2e140a7c
erjigit12 <bvjcjcgjf@gmail.com>
2025-02-06 16:05:25 +0600

    edit recorrected

--- lib/src/feature/profile/presentation/pages/edit_profile_page.dart
+++ lib/src/feature/profile/presentation/pages/edit_profile_page.dart
@@ -5,6 +5,10 @@ class EditProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
