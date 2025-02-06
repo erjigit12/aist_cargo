d2c204d6dc0d6385a10cd8456451c6d69e7ca1f3
erjigit12 <bvjcjcgjf@gmail.com>
2025-02-06 16:04:42 +0600

    edit recorrected

--- lib/src/feature/profile/presentation/pages/edit_profile_page.dart
+++ lib/src/feature/profile/presentation/pages/edit_profile_page.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
