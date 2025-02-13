d3d1f8348ddf31188b2276e65ac53ffa430ea06e
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-02-14 01:16:40 +0600

    router added aa

--- lib/src/feature/profile/presentation/pages/my_subscription_page.dart
+++ lib/src/feature/profile/presentation/pages/my_subscription_page.dart
@@ -1,4 +1,4 @@
-import 'package:aist_cargo/src/core/components/custom_app_bar.dart';
+import 'package:aist_cargo/src/core/core.dart';
 import 'package:flutter/material.dart';
 
 class MySubscriptionPage extends StatelessWidget {
@@ -13,6 +13,35 @@ class MySubscriptionPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: appBarTitle),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/dollars.png'),
+          const Text('1 месяц за 99₽', style: AppTextStyles.f16w600),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Дата начала', style: AppTextStyles.f14w500),
+                Text('10.01.2025', style: AppTextStyles.f16w600),
+              ],
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Дата окончания', style: AppTextStyles.f14w500),
+                Text('10.02.2025', style: AppTextStyles.f16w600),
+              ],
+            ),
+          ),
+          // ElevatedButtonWidget(title: 'Обновить  подписку', onPressed: () {}),
+        ],
+      ),
     );
   }
 }
