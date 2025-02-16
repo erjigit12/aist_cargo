a3eed685bea5451af0e15da600aa32d33356ecf7
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-02-16 17:37:36 +0600

    user entity

--- lib/src/feature/auth/domain/entities/user_entity.dart
+++ lib/src/feature/auth/domain/entities/user_entity.dart
@@ -1,1 +1,15 @@
-class UserEntity {}
+class UserEntity {
+  final String? id;
+  final String? name;
+  final String? surName;
+  final String? email;
+  final String? password;
+
+  UserEntity({
+    this.id,
+    this.name,
+    this.surName,
+    this.email,
+    this.password,
+  });
+}
