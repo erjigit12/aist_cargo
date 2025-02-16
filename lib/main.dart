879568cd5037f58368bb26a7ebea64d27cf2f1d2
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-02-16 17:32:51 +0600

    somethings updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:flutter/material.dart';
 import 'injection_container.dart' as di;
+import 'src/core/constant/generated/strings.g.dart';
 
 Future<void> main() async {
   await di.init();
 
+  WidgetsFlutterBinding.ensureInitialized(); // add this
+  LocaleSettings.useDeviceLocale();
   runApp(const MyApp());
 }
