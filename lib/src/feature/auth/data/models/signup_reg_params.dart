05082471f381f451943af012aa3493db48e191e2
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-02-17 16:11:31 +0600

    updated something

---
+++ lib/src/feature/auth/data/models/signup_reg_params.dart
@@ -0,0 +1,15 @@
+class SignupRegParams {
+  final String? id;
+  final String? name;
+  final String? surName;
+  final String? email;
+  final String? password;
+
+  SignupRegParams({
+    this.id,
+    this.name,
+    this.surName,
+    this.email,
+    this.password,
+  });
+}
