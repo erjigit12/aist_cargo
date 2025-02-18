a1089cb26a30021c7818ecc577aa510a6ec8a9b5
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-02-18 18:22:54 +0600

    bloc add

--- lib/src/feature/auth/presentation/logic/credential/credential_state.dart
+++ lib/src/feature/auth/presentation/logic/credential/credential_state.dart
@@ -8,3 +8,16 @@ sealed class CredentialState extends Equatable {
 }
 
 final class CredentialInitial extends CredentialState {}
+
+final class CredentialLoading extends CredentialState {}
+
+final class CredentialSuccess extends CredentialState {}
+
+final class CredentialFailure extends CredentialState {
+  const CredentialFailure({required this.errorMessage});
+
+  final String errorMessage;
+
+  @override
+  List<Object> get props => [errorMessage];
+}
