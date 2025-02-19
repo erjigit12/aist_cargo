76b5881adb48d27f4a6d50f2b643ce3eba82eda3
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-02-19 13:02:45 +0600

    routes added

--- lib/src/core/utils/routes/app_routes.dart
+++ lib/src/core/utils/routes/app_routes.dart
@@ -1,24 +1,24 @@
 class AppRoutes {
   /// The Initial Page
-  static const main = '/main';
-  static const onboarding = '/onboarding';
+  static const main = 'main';
+  static const onboarding = 'onboarding';
 
   /* <---- Login, Signup -----> */
   static const login = 'login';
-  static const signup = '/signup';
-  static const forgotPassword = '/forgotPassword';
-  static const otpCode = '/otpCode';
+  static const signup = 'signup';
+  static const forgotPassword = 'forgotPassword';
+  static const otpCode = 'otpCode';
 
   /* <---- Delivery -----> */
-  static const createDelivery = '/createDelivery';
-  static const addCard = '/addCardPage';
-  static const senderSubscription = '/senderSubscription';
-  static const placeOrder = '/placeOrder';
+  static const createDelivery = 'createDelivery';
+  static const addCard = 'addCardPage';
+  static const senderSubscription = 'senderSubscription';
+  static const placeOrder = 'placeOrder';
 
   /*  <---- All -----> */
-  static const tripsOrParcels = '/tripsOrParcels';
+  static const tripsOrParcels = 'tripsOrParcels';
 
   /*  <---- Profile -----> */
-  static const editProfile = '/editProfile';
-  static const mySubscription = '/mySubscription';
+  static const editProfile = 'editProfile';
+  static const mySubscription = 'mySubscription';
 }
