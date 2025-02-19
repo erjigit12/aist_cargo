330d02e1bf4a40f871876914ac2434485c10d3c8
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-02-19 11:24:40 +0600

    local auth formatted

--- lib/src/feature/auth/data/data_sources/local/local_auth_data_source_impl.dart
+++ lib/src/feature/auth/data/data_sources/local/local_auth_data_source_impl.dart
@@ -1,14 +1,20 @@
 import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:dartz/dartz.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalAuthDataSourceImpl implements LocalAuthDataSource {
   @override
   Future<bool> isLoggedIn() async {
-    throw UnimplementedError();
+    SharedPreferences storage = await SharedPreferences.getInstance();
+    var accessToken = storage.getString('accessToken');
+    if (accessToken == null) return false;
+    return true;
   }
 
   @override
   Future<Either> logOut() async {
-    throw UnimplementedError();
+    SharedPreferences storage = await SharedPreferences.getInstance();
+    await storage.clear();
+    return const Right(true);
   }
 }
