2a0d9bf7498ab0473653fd97b9ce41cd9adb9c12
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-03-03 11:54:15 +0600

    signup recorecte

--- lib/src/feature/auth/presentation/pages/signup_page.dart
+++ lib/src/feature/auth/presentation/pages/signup_page.dart
@@ -11,6 +11,8 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+
   final _firstNameController = TextEditingController();
   final _lastNameController = TextEditingController();
   final _emailController = TextEditingController();
@@ -50,162 +52,165 @@ class _SignUpPageState extends State<SignUpPage> {
   Padding _bodyWidget(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: Image(
-                image: AssetImage('assets/images/logo_aist_cargo.png'),
+      child: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: Image(
+                  image: AssetImage('assets/images/logo_aist_cargo.png'),
+                ),
               ),
-            ),
-            16.h,
-            const Center(
-              child: Image(
-                image: AssetImage('assets/images/aistcargo.png'),
+              16.h,
+              const Center(
+                child: Image(
+                  image: AssetImage('assets/images/aistcargo.png'),
+                ),
               ),
-            ),
-            16.h,
-            const Text(
-              'AistCargo - быстро и удобно передать посылку между доставщиком и отправителем',
-              style: AppTextStyles.f14w400,
-              textAlign: TextAlign.center,
-            ),
-            16.h,
-            const Center(
-              child: Text(
-                'Регистрация',
-                style: AppTextStyles.f20w400,
+              16.h,
+              const Text(
+                'AistCargo - быстро и удобно передать посылку между доставщиком и отправителем',
+                style: AppTextStyles.f14w400,
                 textAlign: TextAlign.center,
               ),
-            ),
-            16.h,
-            Row(
-              children: [
-                Expanded(
-                  child: OutlineButtonWidget(
-                    title: 'Google',
-                    icon: 'assets/icons/google.png',
-                    onPressed: () {},
+              16.h,
+              const Center(
+                child: Text(
+                  'Регистрация',
+                  style: AppTextStyles.f20w400,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              16.h,
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlineButtonWidget(
+                      title: 'Google',
+                      icon: 'assets/icons/google.png',
+                      onPressed: () {},
+                    ),
                   ),
-                ),
-                8.w,
-                Expanded(
-                  child: OutlineButtonWidget(
-                    title: 'Apple',
-                    icon: 'assets/icons/apple.png',
-                    onPressed: () {},
+                  8.w,
+                  Expanded(
+                    child: OutlineButtonWidget(
+                      title: 'Apple',
+                      icon: 'assets/icons/apple.png',
+                      onPressed: () {},
+                    ),
                   ),
-                ),
-              ],
-            ),
-            16.h,
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Divider(
-                    thickness: 0.8,
-                    color: AppColors.greyBrightColor,
+                ],
+              ),
+              16.h,
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.8,
+                      color: AppColors.greyBrightColor,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text('Или войти с помощью'),
-                ),
-                Expanded(
-                  child: Divider(
-                    thickness: 0.8,
-                    color: AppColors.greyBrightColor,
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text('Или войти с помощью'),
                   ),
-                ),
-              ],
-            ),
-            32.h,
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.8,
+                      color: AppColors.greyBrightColor,
+                    ),
+                  ),
+                ],
+              ),
+              32.h,
 
-            // TEXT FIELDS
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('Имя', style: AppTextStyles.f14w500),
-                8.h,
-                TextFormFieldWidget(
-                  controller: _firstNameController,
-                  hintText: 'Ivan',
-                ),
-                16.h,
-                const Text('Фамилия', style: AppTextStyles.f14w500),
-                8.h,
-                TextFormFieldWidget(
-                  controller: _lastNameController,
-                  hintText: 'Ivanov',
-                ),
-                16.h,
-                const Text('e-mail', style: AppTextStyles.f14w500),
-                8.h,
-                TextFormFieldWidget(
-                  controller: _emailController,
-                  hintText: 'Ivan@gmail.com',
-                ),
-                16.h,
-                const Text('Пароль', style: AppTextStyles.f14w500),
-                8.h,
-                TextFormFieldWidget(
-                  controller: _passwordController,
-                  suffix: true,
-                  hintText: '******',
-                ),
-                16.h,
-                const Text(
-                  'Подтвердите пароль',
-                  style: AppTextStyles.f14w500,
-                ),
-                8.h,
-                TextFormFieldWidget(
-                  controller: _confirmPasswordController,
-                  suffix: true,
-                  hintText: '******',
-                ),
-                26.h,
-                BlocBuilder<CredentialCubit, CredentialState>(
-                  builder: (context, state) {
-                    if (state is CredentialLoading) {
-                      return const Center(
-                        child: CircularProgressIndicator(
-                          color: Colors.black,
-                        ),
-                      );
-                    }
-                    return ElevatedButtonWidget(
-                        title: 'Продолжить',
-                        onPressed: () async {
-                          if (_passwordController.text !=
-                              _confirmPasswordController.text) {
-                            var snackBar = const SnackBar(
-                                content: Text('Пароли не совпадают'));
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(snackBar);
-                            return;
-                          } else {
-                            context
-                                .read<CredentialCubit>()
-                                .signUp(SignupRegParams(
-                                  firstName: _firstNameController.text,
-                                  lastName: _lastNameController.text,
-                                  email: _emailController.text,
-                                  password: _passwordController.text,
-                                ));
-                          }
-                        });
-                  },
-                ),
-                8.h,
-                const UserAgreement(),
-              ],
-            ),
-          ],
+              // TEXT FIELDS
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Имя', style: AppTextStyles.f14w500),
+                  8.h,
+                  TextFormFieldWidget(
+                    controller: _firstNameController,
+                    hintText: 'Ivan',
+                  ),
+                  16.h,
+                  const Text('Фамилия', style: AppTextStyles.f14w500),
+                  8.h,
+                  TextFormFieldWidget(
+                    controller: _lastNameController,
+                    hintText: 'Ivanov',
+                  ),
+                  16.h,
+                  const Text('e-mail', style: AppTextStyles.f14w500),
+                  8.h,
+                  TextFormFieldWidget(
+                    controller: _emailController,
+                    hintText: 'Ivan@gmail.com',
+                  ),
+                  16.h,
+                  const Text('Пароль', style: AppTextStyles.f14w500),
+                  8.h,
+                  TextFormFieldWidget(
+                    controller: _passwordController,
+                    suffix: true,
+                    hintText: '******',
+                  ),
+                  16.h,
+                  const Text(
+                    'Подтвердите пароль',
+                    style: AppTextStyles.f14w500,
+                  ),
+                  8.h,
+                  TextFormFieldWidget(
+                    controller: _confirmPasswordController,
+                    suffix: true,
+                    hintText: '******',
+                  ),
+                  26.h,
+                  BlocBuilder<CredentialCubit, CredentialState>(
+                    builder: (context, state) {
+                      if (state is CredentialLoading) {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.black,
+                          ),
+                        );
+                      }
+                      return ElevatedButtonWidget(
+                          title: 'Продолжить',
+                          onPressed: () async {
+                            if (_passwordController.text !=
+                                _confirmPasswordController.text) {
+                              var snackBar = const SnackBar(
+                                  content: Text('Пароли не совпадают'));
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                              return;
+                            } else {
+                              context
+                                  .read<CredentialCubit>()
+                                  .signUp(SignupRegParams(
+                                    firstName: _firstNameController.text,
+                                    lastName: _lastNameController.text,
+                                    email: _emailController.text,
+                                    password: _passwordController.text,
+                                  ));
+                            }
+                          });
+                    },
+                  ),
+                  8.h,
+                  const UserAgreement(),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
