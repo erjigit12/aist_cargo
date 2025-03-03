a551d2b027251a7afb0b91470f8f079da1dc3f52
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-03-03 12:12:34 +0600

    signup full recorected

--- lib/src/feature/auth/presentation/pages/login_page.dart
+++ lib/src/feature/auth/presentation/pages/login_page.dart
@@ -41,172 +41,175 @@ class _LoginPageState extends State<LoginPage> {
       key: _formKey,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: Image(
-                image: AssetImage('assets/images/logo_aist_cargo.png'),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: Image(
+                  image: AssetImage('assets/images/logo_aist_cargo.png'),
+                ),
               ),
-            ),
-            16.h,
-            const Center(
-              child: Image(
-                image: AssetImage('assets/images/aistcargo.png'),
+              16.h,
+              const Center(
+                child: Image(
+                  image: AssetImage('assets/images/aistcargo.png'),
+                ),
               ),
-            ),
-            16.h,
-            const Text(
-              'AistCargo - быстро и удобно передать посылку между доставщиком и отправителем',
-              style: AppTextStyles.f14w400,
-              textAlign: TextAlign.center,
-            ),
-            16.h,
-            const Center(
-              child: Text(
-                'Вход',
-                style: AppTextStyles.f20w400,
+              16.h,
+              const Text(
+                'AistCargo - быстро и удобно передать посылку между доставщиком и отправителем',
+                style: AppTextStyles.f14w400,
                 textAlign: TextAlign.center,
               ),
-            ),
-            16.h,
-            Row(
-              children: [
-                Expanded(
-                  child: OutlineButtonWidget(
-                    title: 'Google',
-                    icon: 'assets/icons/google.png',
-                    onPressed: () {},
+              16.h,
+              const Center(
+                child: Text(
+                  'Вход',
+                  style: AppTextStyles.f20w400,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              16.h,
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlineButtonWidget(
+                      title: 'Google',
+                      icon: 'assets/icons/google.png',
+                      onPressed: () {},
+                    ),
                   ),
-                ),
-                8.w,
-                Expanded(
-                  child: OutlineButtonWidget(
-                    title: 'Apple',
-                    icon: 'assets/icons/apple.png',
-                    onPressed: () {},
+                  8.w,
+                  Expanded(
+                    child: OutlineButtonWidget(
+                      title: 'Apple',
+                      icon: 'assets/icons/apple.png',
+                      onPressed: () {},
+                    ),
                   ),
-                ),
-              ],
-            ),
-            16.h,
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Divider(
-                    thickness: 0.8,
-                    color: AppColors.greyBrightColor,
+                ],
+              ),
+              16.h,
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.8,
+                      color: AppColors.greyBrightColor,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: Text('Или войти с помощью'),
-                ),
-                Expanded(
-                  child: Divider(
-                    thickness: 0.8,
-                    color: AppColors.greyBrightColor,
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text('Или войти с помощью'),
                   ),
-                ),
-              ],
-            ),
-            32.h,
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.8,
+                      color: AppColors.greyBrightColor,
+                    ),
+                  ),
+                ],
+              ),
+              32.h,
 
-            // TEXT FIELDS
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFieldWithTitle(
-                  controller: _emailController,
-                  title: 'Электронная почта',
-                  backgroundColor: Colors.transparent,
-                  hintText: 'Электронная почта',
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (val) {
-                    if (val!.isValidEmail == false) {
-                      return "Enter a valid email";
-                    }
-                    return null;
-                  },
-                ),
-                16.h,
-                TextFieldWithTitle(
-                  controller: _passwordController,
-                  title: 'Пароль',
-                  backgroundColor: Colors.transparent,
-                  hintText: 'Пароль',
-                  keyboardType: TextInputType.visiblePassword,
-                  suffix: true,
-                  validator: (val) {
-                    if (val!.isValidPassword == false) {
-                      return "Enter a valid password";
-                    }
-                    return null;
-                  },
-                ),
-                8.h,
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      onTap: () =>
-                          Navigator.pushNamed(context, 'forgotPassword'),
+              // TEXT FIELDS
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFieldWithTitle(
+                    controller: _emailController,
+                    title: 'Электронная почта',
+                    backgroundColor: Colors.transparent,
+                    hintText: 'Электронная почта',
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (val) {
+                      if (val!.isValidEmail == false) {
+                        return "Enter a valid email";
+                      }
+                      return null;
+                    },
+                  ),
+                  16.h,
+                  TextFieldWithTitle(
+                    controller: _passwordController,
+                    title: 'Пароль',
+                    backgroundColor: Colors.transparent,
+                    hintText: 'Пароль',
+                    keyboardType: TextInputType.visiblePassword,
+                    suffix: true,
+                    validator: (val) {
+                      if (val!.isValidPassword == false) {
+                        return "Enter a valid password";
+                      }
+                      return null;
+                    },
+                  ),
+                  8.h,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () =>
+                            Navigator.pushNamed(context, 'forgotPassword'),
+                        child: Text(
+                          'Забыли пароль?',
+                          style: AppTextStyles.f14w500.copyWith(
+                            color: AppColors.buttonColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  16.h,
+                  BlocBuilder<CredentialCubit, CredentialState>(
+                    builder: (context, state) {
+                      if (state is CredentialLoading) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return ElevatedButtonWidget(
+                          title: 'Продолжить',
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              context.read<CredentialCubit>().signIn(
+                                    SigninRegParams(
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                    ),
+                                  );
+                            }
+                          });
+                    },
+                  ),
+                  8.h,
+                  const UserAgreement(),
+                  26.h,
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, AppRoutes.signup);
+                      },
                       child: Text(
-                        'Забыли пароль?',
+                        'Регистрация',
                         style: AppTextStyles.f14w500.copyWith(
-                          color: AppColors.buttonColor,
+                          color: AppColors.blueColor,
+                          decoration: TextDecoration.underline,
+                          decorationColor: AppColors.blueColor,
                         ),
                       ),
                     ),
-                  ],
-                ),
-                16.h,
-                BlocBuilder<CredentialCubit, CredentialState>(
-                  builder: (context, state) {
-                    if (state is CredentialLoading) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    return ElevatedButtonWidget(
-                        title: 'Продолжить',
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            context.read<CredentialCubit>().signIn(
-                                  SigninRegParams(
-                                    email: _emailController.text,
-                                    password: _passwordController.text,
-                                  ),
-                                );
-                          }
-                        });
-                  },
-                ),
-                8.h,
-                const UserAgreement(),
-                26.h,
-                Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, AppRoutes.signup);
-                    },
-                    child: Text(
-                      'Регистрация',
-                      style: AppTextStyles.f14w500.copyWith(
-                        color: AppColors.blueColor,
-                        decoration: TextDecoration.underline,
-                        decorationColor: AppColors.blueColor,
-                      ),
-                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
