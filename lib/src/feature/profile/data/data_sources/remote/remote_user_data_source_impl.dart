3d70fb832ba7c5e25813a2063ff28526b37c3f1e
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-03-04 12:54:02 +0600

    user data source

--- lib/src/feature/profile/data/data_sources/remote/remote_user_data_source_impl.dart
+++ lib/src/feature/profile/data/data_sources/remote/remote_user_data_source_impl.dart
@@ -1,9 +1,33 @@
+import 'dart:developer';
+
+import 'package:aist_cargo/injection_container.dart';
+import 'package:aist_cargo/src/core/core.dart';
 import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RemoteUserDataSourceImpl implements RemoteUserDataSource {
   @override
   Future<Either> getUserData(UserModel user) async {
-    return Right(user);
+    try {
+      SharedPreferences storage = await SharedPreferences.getInstance();
+      var token = storage.getString('token');
+      final response = await sl<DioClient>().get(
+        ApiConst.userProfile,
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer $token ',
+          },
+        ),
+      );
+
+      log('Маалымат келди: ${response.data}');
+
+      return Right(response);
+    } on DioException catch (e) {
+      throw Exception(
+          'Failed to get user data: ${e.response?.data ?? e.message}');
+    }
   }
 }
