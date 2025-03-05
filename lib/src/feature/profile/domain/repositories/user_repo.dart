cb634b5f1f7d3e695a6fc1331184839d81b2aef2
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-03-05 16:07:35 +0600

    ui and cubit

--- lib/src/feature/profile/domain/repositories/user_repo.dart
+++ lib/src/feature/profile/domain/repositories/user_repo.dart
@@ -1,5 +1,7 @@
+import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class UserRepository {
   Future<Either> getUserData(int id);
+  Future<Either> updateUserData(UserModel userModel);
 }
