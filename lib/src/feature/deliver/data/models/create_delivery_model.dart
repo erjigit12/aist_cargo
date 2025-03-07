2390fea3493dd1b687120211ef2477cdc8c5d58e
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-03-07 15:10:52 +0600

    model added

--- lib/src/feature/deliver/data/models/create_delivery_model.dart
+++ lib/src/feature/deliver/data/models/create_delivery_model.dart
@@ -1,1 +1,29 @@
-class CreateDeliveryModel {}
+class CreateDeliveryModel {
+  final String? fromWhere;
+  final String? toWhere;
+  final String? description;
+  final String? dispatchDate;
+  final String? arrivalDate;
+  final String? fullName;
+  final String? transportNumber;
+  final String? transportType;
+  final String? packageType;
+  final String? truckSize;
+  final String? size;
+  final String? role;
+
+  CreateDeliveryModel({
+    this.fromWhere,
+    this.toWhere,
+    this.description,
+    this.dispatchDate,
+    this.arrivalDate,
+    this.fullName,
+    this.transportNumber,
+    this.transportType,
+    this.packageType,
+    this.truckSize,
+    this.size,
+    this.role,
+  });
+}
