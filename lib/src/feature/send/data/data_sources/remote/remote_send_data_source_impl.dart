d86ba2bedfa5cee9cbc28b5a51e7661fe857acdc
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-03-14 18:24:11 +0600

    some things changes

--- lib/src/feature/send/data/data_sources/remote/remote_send_data_source_impl.dart
+++ lib/src/feature/send/data/data_sources/remote/remote_send_data_source_impl.dart
@@ -1,20 +1,39 @@
+import 'dart:developer';
+
 import 'package:aist_cargo/injection_container.dart';
 import 'package:aist_cargo/src/core/core.dart';
 import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RemoteSendDataSourceImpl implements RemoteSendDataSource {
   @override
   Future<Either> createSend(SendModel sendModel) async {
     try {
+      SharedPreferences storage = await SharedPreferences.getInstance();
+      var accessToken = storage.getString('accessToken');
+
+      log("✅ Access Token: $accessToken");
+
       final response = await sl<DioClient>().post(
         ApiConst.createSendings,
-        data: sendModel.toMap(),
+        options: Options(
+          headers: {
+            'accept': ' */*',
+            'Authorization': 'Bearer $accessToken',
+            'Content-Type': 'application/json',
+          },
+        ),
+        data: sendModel.toJson(),
       );
       return Right(response.data);
+    } on DioException catch (e) {
+      log('❌ Сервер ката берди: ${e.response ?? e.message}');
+      throw Exception('Failed to create delivery: ${e.response}');
     } catch (e) {
-      throw Exception(
-          'Failed to create send: ${e.response?.data ?? e.message}');
+      log('❌ Жалпы ката: $e');
+      throw Exception('Unexpected error: $e');
     }
   }
 }
