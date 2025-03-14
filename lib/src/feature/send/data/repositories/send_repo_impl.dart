b5b466dee0263d342674428b8c7071711dc75bec
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-03-14 18:17:02 +0600

    some things changes

--- lib/src/feature/send/data/repositories/send_repo_impl.dart
+++ lib/src/feature/send/data/repositories/send_repo_impl.dart
@@ -2,9 +2,12 @@ import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:dartz/dartz.dart';
 
 class SendRepositoryImpl implements SendRepository {
+  SendRepositoryImpl({required this.remoteSendDataSource});
+
+  final RemoteSendDataSource remoteSendDataSource;
+
   @override
-  Future<Either> createSend(SendModel sendModel) {
-    // TODO: implement createSend
-    throw UnimplementedError();
+  Future<Either> createSend(SendModel sendModel) async {
+    return await remoteSendDataSource.createSend(sendModel);
   }
 }
