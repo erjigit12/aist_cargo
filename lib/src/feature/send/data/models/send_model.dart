e0a5ee4938d9a7b87001fde3cad16f4ed3ded391
ERZHIGIT M <bvjcjcgjf@gmail.com>
2025-03-14 17:38:38 +0600

    models added

---
+++ lib/src/feature/send/data/models/send_model.dart
@@ -0,0 +1,25 @@
+class SendModel {
+  final String? firstName;
+  final String? lastName;
+  final String? fromWhere;
+  final String? toWhere;
+  final String? description;
+  final String? dispatchDate;
+  final String? arrivalDate;
+  final String? packageType;
+  final String? size;
+  final String? subsDuration;
+
+  SendModel({
+    this.firstName,
+    this.lastName,
+    this.fromWhere,
+    this.toWhere,
+    this.description,
+    this.dispatchDate,
+    this.arrivalDate,
+    this.packageType,
+    this.size,
+    this.subsDuration,
+  });
+}
