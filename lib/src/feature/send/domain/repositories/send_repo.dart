daf064ab4763e543268ca819609de18664ba3736
mamarasulov <bvjcjcgjf@gmail.com>
2025-03-27 11:26:30 +0600

    to do delivery fully

--- lib/src/feature/send/domain/repositories/send_repo.dart
+++ lib/src/feature/send/domain/repositories/send_repo.dart
@@ -2,5 +2,5 @@ import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class SendRepository {
-  Future<Either> createSend(CreateDeliveryModel sendModel);
+  Future<Either> isSubscribed(CreateDeliveryModel sendModel);
 }
