45ff5d87dab92113be9c5796ad8a97d630f0b6d7
mamarasulov <bvjcjcgjf@gmail.com>
2025-04-11 12:11:22 +0600

    all page Ин Шаа Аллах will let's go

--- lib/src/feature/all/presentation/logic/all_cubit.dart
+++ lib/src/feature/all/presentation/logic/all_cubit.dart
@@ -1,8 +1,21 @@
+import 'package:aist_cargo/src/feature/feature.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'all_state.dart';
 
 class AllCubit extends Cubit<AllState> {
-  AllCubit() : super(AllInitial());
+  final GetDeliveryByIdUsecase getDeliveryByIdUsecase;
+
+  AllCubit({required this.getDeliveryByIdUsecase}) : super(AllInitial());
+
+  Future<void> fetchDelivery(int id) async {
+    emit(DeliveryLoading());
+    try {
+      final delivery = await getDeliveryByIdUsecase(id);
+      emit(DeliveryLoaded(delivery));
+    } catch (e) {
+      emit(DeliveryError(e.toString()));
+    }
+  }
 }
--- lib/src/feature/all/presentation/logic/all_state.dart
+++ lib/src/feature/all/presentation/logic/all_state.dart
@@ -8,3 +8,23 @@ sealed class AllState extends Equatable {
 }
 
 final class AllInitial extends AllState {}
+
+final class DeliveryLoading extends AllState {}
+
+final class DeliveryLoaded extends AllState {
+  final DeliveryEntity delivery;
+
+  const DeliveryLoaded(this.delivery);
+
+  @override
+  List<Object> get props => [delivery];
+}
+
+final class DeliveryError extends AllState {
+  final String message;
+
+  const DeliveryError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
