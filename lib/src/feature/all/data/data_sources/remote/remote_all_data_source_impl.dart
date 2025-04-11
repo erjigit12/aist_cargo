fb61600d36fe7cc691087c96fac3c2c3f04c14af
mamarasulov <bvjcjcgjf@gmail.com>
2025-04-11 12:08:51 +0600

    all page Ин Шаа Аллах will let's go

--- lib/src/feature/all/data/data_sources/remote/remote_all_data_source_impl.dart
+++ lib/src/feature/all/data/data_sources/remote/remote_all_data_source_impl.dart
@@ -1,3 +1,14 @@
+import 'package:aist_cargo/injection_container.dart';
+import 'package:aist_cargo/src/core/core.dart';
+import 'package:aist_cargo/src/feature/all/data/models/get_delivery_by_id.dart';
 import 'package:aist_cargo/src/feature/feature.dart';
 
-class RemoteAllDataSourceImpl implements RemoteAllDataSource {}
+class RemoteAllDataSourceImpl implements RemoteAllDataSource {
+  @override
+  Future<DeliveryEntity> getDeliveryById(int id) async {
+    final response = await sl<DioClient>().get(
+      '${ApiConst.getDeliveryById}/$id',
+    );
+    return GetDeliveryById.fromJson(response.data);
+  }
+}
